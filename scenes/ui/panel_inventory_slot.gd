17aee0b86ade0f01baea9ed4831d144c411fab58
GoDie910 <diego_shot100@hotmail.com>
2025-08-26 17:42:35 -0500

    inventory slot basic 2

--- scenes/ui/panel_inventory_slot.gd
+++ scenes/ui/panel_inventory_slot.gd
@@ -12,6 +12,7 @@ func place(item: Item) -> bool:
 		return false
 	_item = item
 	texture_rect_item.texture = _item.texture
+	texture_rect_item.tooltip_text = _item.description
 	texture_rect_item.visible = true
 	return true
 
@@ -19,6 +20,7 @@ func take() -> Item:
 	if _item == null:
 		return null
 	texture_rect_item.texture = null
+	texture_rect_item.tooltip_text = ""
 	texture_rect_item.visible = false
 	var aux_item: Item = _item
 	_item = null
@@ -32,7 +34,13 @@ func use(event: InputEvent) -> void:
 	if !event.pressed:
 		return
 	#code for using the item
-	#code for telling the game_manager we've used the item
+	#code for telling the game_manager we've used the itemx
 	print("using item ", _item.name_on_screen)
 	take()
 	pass
+
+func _on_item_mouse_entered() -> void:
+	texture_rect_item.modulate = Color(1, 1, 1, 0.7)
+
+func _on_item_mouse_exited() -> void:
+	texture_rect_item.modulate = Color(1, 1, 1, 1)
