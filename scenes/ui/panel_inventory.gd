cd8ebaf7834d478fafd65d59dd4d5c43d8d8d952
GoDie910 <diego_shot100@hotmail.com>
2025-08-26 17:29:14 -0500

    inventory slot basic

--- scenes/ui/panel_inventory.gd
+++ scenes/ui/panel_inventory.gd
@@ -1,3 +1,22 @@
 extends Panel
 
-class_name PanelInventory
+class_name UIInventory
+
+@export var items: Array[Item]
+@onready var slots: Array[UIInventorySlot] = [$Slot,$Slot2,$Slot3,$Slot4,$Slot5]
+
+func _ready() -> void:
+	var item_dups: Array[Item] = []
+	item_dups.resize(items.size())
+	for i in range(items.size()):
+		item_dups[i] = items[i].duplicate(true)
+	inventory_place(item_dups)
+
+func inventory_place(inventory: Array[Item]) -> void:
+	for i in range(slots.size()):
+		if i >= inventory.size():
+			return
+		if inventory[i] == null:
+			return
+		print("slot: ", slots[i]," & item: ", inventory[i])
+		slots[i].place(inventory[i])
---
+++ scenes/ui/panel_inventory_slot.gd
@@ -0,0 +1,38 @@
+extends Control
+
+class_name UIInventorySlot
+
+@onready var texture_rect_border: TextureRect = $Border
+@onready var texture_rect_item: TextureRect = $Item
+
+var _item: Item = null
+
+func place(item: Item) -> bool:
+	if _item != null:
+		return false
+	_item = item
+	texture_rect_item.texture = _item.texture
+	texture_rect_item.visible = true
+	return true
+
+func take() -> Item:
+	if _item == null:
+		return null
+	texture_rect_item.texture = null
+	texture_rect_item.visible = false
+	var aux_item: Item = _item
+	_item = null
+	return aux_item
+
+func use(event: InputEvent) -> void:
+	if event is not InputEventMouseButton:
+		return
+	if event.button_index != MOUSE_BUTTON_LEFT:
+		return
+	if !event.pressed:
+		return
+	#code for using the item
+	#code for telling the game_manager we've used the item
+	print("using item ", _item.name_on_screen)
+	take()
+	pass
