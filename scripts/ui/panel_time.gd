268447726378c7f3764143e88c4164f231ffb287
GoDie910 <diego_shot100@hotmail.com>
2025-08-27 16:49:14 -0500

    imported ui assets - 2

--- scripts/ui/panel_time.gd
+++ scripts/ui/panel_time.gd
@@ -5,8 +5,8 @@ class_name UITime
 @onready var wheel: TextureRect = $Wheel
 @onready var pin: TextureRect = $Pin
 
-var rotation_step: float = 22.5
-var rotation_duration: float = 480.0
+var rotation_step: float = 45.0
+var rotation_duration: float = 80.0
 var elapsed_time: float = 0.0
 var is_rotating: bool = false
 
@@ -15,7 +15,6 @@ signal on_time_end
 func start(time_until_lose: float) -> void:
 	rotation_duration = time_until_lose
 	elapsed_time = 0.0
-	wheel.rotation_degrees = rotation_step
 	is_rotating = true
 
 func pause() -> void:
@@ -33,17 +32,17 @@ func _wheel_rotate(delta: float) -> void:
 	
 	if elapsed_time >= rotation_duration:
 		elapsed_time = rotation_duration
-		wheel.rotation_degrees = rotation_step + (360.0 - rotation_step)
+		wheel.rotation_degrees = 360.0 - rotation_step
 		is_rotating = false
 		on_time_end.emit()
 	else:
-		wheel.rotation_degrees = rotation_step + (elapsed_time / rotation_duration) * (360.0 - rotation_step)
+		wheel.rotation_degrees = (elapsed_time / rotation_duration) * (360.0 - rotation_step)
 
 func shift_time(seconds: float) -> void:
 	var previous_elapsed = elapsed_time
 	elapsed_time += seconds
 	elapsed_time = clamp(elapsed_time, 0.0, rotation_duration)
-	wheel.rotation_degrees = rotation_step + (elapsed_time / rotation_duration) * (360.0 - rotation_step)
+	wheel.rotation_degrees = (elapsed_time / rotation_duration) * (360.0 - rotation_step)
 	if previous_elapsed < rotation_duration && elapsed_time >= rotation_duration:
 		is_rotating = false
 		on_time_end.emit()
